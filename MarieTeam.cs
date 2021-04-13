e5b8a7e9c3f9196af825ba0d90fe00536c957b7a
vvuylsteker <vvuylsteker@gmail.com>
2021-04-13 23:42:53 +0200

    edit

--- MarieTeam.cs
+++ MarieTeam.cs
@@ -36,8 +36,6 @@ namespace client_marieteam
     {
         string pathimage { get; set; }
         float vitesse { get; set; }
-        MySqlConnection client { get; set; }
-
         List<string> equipements { get; set; } = new List<string>();
         public BateauVoyageur(int id, string nom, float longueur, float largeur, string pathimage, float vitesse) : base(id, nom, longueur, largeur)
         {
