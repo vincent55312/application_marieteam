e45782526af597c4ec0dee162df5ff6f5963411c
vvuylsteker <vvuylsteker@gmail.com>
2021-04-13 21:36:03 +0200

    fr to en

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,20 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
-using PdfSharp.Pdf;
-using PdfSharp.Drawing;
-using System.Diagnostics;
-using PdfSharp.Drawing.Layout;
-
 
 namespace client_marieteam
 {
@@ -32,8 +19,7 @@ namespace client_marieteam
             maincanvas.Visibility = Visibility.Hidden;
 
         }
-
-        private void Button_connection_Click(object sender, RoutedEventArgs e)
+        private void clickConnect(object sender, RoutedEventArgs e)
         {
             ClientSQL client = new ClientSQL();
             if (client.OpenConnection())
@@ -42,45 +28,7 @@ namespace client_marieteam
                 maincanvas.Visibility = Visibility.Visible;
             }
         }
-
-        private void sortie_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new Microsoft.Win32.SaveFileDialog();
-            dialog.InitialDirectory = TextboxSortie.Text;
-            dialog.Title = "Select a Directory"; 
-            dialog.Filter = "Directory|*.this.directory"; 
-            dialog.FileName = "select";
-            if (dialog.ShowDialog() == true)
-            {
-                string path = dialog.FileName;
-                path = path.Replace("\\select.this.directory", "");
-                path = path.Replace(".this.directory", "");
-                if (!System.IO.Directory.Exists(path))
-                {
-                    System.IO.Directory.CreateDirectory(path);
-                }
-                TextboxSortie.Text = path + "\\MarieTeam.pdf";
-            }
-        }
-        private void CollectEditeur(object sender, RoutedEventArgs e)
-        {
-            ClientSQL client = new ClientSQL();
-            if (client.OpenConnection())
-            {
-                var sql = "SELECT * FROM bateau";
-                var cmd = new MySqlCommand(sql, client.Client);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-                Passerelle Bateaux = new Passerelle();
-
-                while (rdr.Read())
-                {
-                    Bateaux.bateauVoyageurs.Add(new BateauVoyageur(rdr.GetInt32(0), rdr.GetString(1), rdr.GetFloat(2), rdr.GetFloat(3), rdr.GetString(4), rdr.GetFloat(5)));
-                }
-                client.CloseConnection();
-                foreach (var item in Bateaux.bateauVoyageurs) maintextbox.Text += item.ToString()+ "\n#NEWPAGE";
-            }
-        }
-        private void GenererPDF(object sender, RoutedEventArgs e)
+        private void clickGeneratePDF(object sender, RoutedEventArgs e)
         {
             if (TextboxSortie.Text.Length != 0)
             {
@@ -95,10 +43,48 @@ namespace client_marieteam
                 MessageBox.Show("Veuillez saisir un repertoire de sortie");
             }
         }
-
-        private void ResetEditeur(object sender, RoutedEventArgs e)
+        private void clickResetEditor(object sender, RoutedEventArgs e)
         {
             maintextbox.Text = "";
+        }
+
+        private void clickOutput(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.InitialDirectory = TextboxSortie.Text;
+            dialog.Title = "Select a Directory";
+            dialog.Filter = "Directory|*.this.directory";
+            dialog.FileName = "select";
+            if (dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+                path = path.Replace("\\select.this.directory", "");
+                path = path.Replace(".this.directory", "");
+                if (!System.IO.Directory.Exists(path))
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+                TextboxSortie.Text = path + "\\MarieTeam.pdf";
+            }
+        }
+
+        private void clickGetEditor(object sender, RoutedEventArgs e)
+        {
+            ClientSQL client = new ClientSQL();
+            if (client.OpenConnection())
+            {
+                var sql = "SELECT * FROM bateau";
+                var cmd = new MySqlCommand(sql, client.Client);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                Passerelle Bateaux = new Passerelle();
+
+                while (rdr.Read())
+                {
+                    Bateaux.bateauVoyageurs.Add(new BateauVoyageur(rdr.GetInt32(0), rdr.GetString(1), rdr.GetFloat(2), rdr.GetFloat(3), rdr.GetString(4), rdr.GetFloat(5)));
+                }
+                client.CloseConnection();
+                foreach (var item in Bateaux.bateauVoyageurs) maintextbox.Text += item.ToString() + "\n#NEWPAGE";
+            }
         }
     }
 }
