bd08f04817dec4d38b638ddd4e8005a11983fb74
vvuylsteker <vvuylsteker@gmail.com>
2021-04-15 17:21:04 +0200

    tests

---
+++ TEST/ParsingTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using client_marieteam;
+
+namespace Tests_client_marieteam
+{
+    [TestClass]
+    public class ParsingTests
+    {
+        [TestMethod]
+        public void TestParsingImage()
+        {
+            
+        }
+    }
+}
---
+++ TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TESTS_clientmarieteam")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TESTS_clientmarieteam")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89e3558f-7849-489d-a815-bc91b31ccb97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
